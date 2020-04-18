3364a2b19cbf6a9039bb9da3840768335bbc7e5a
Carlos Morales <carlosm3415@gmail.com>
2020-04-17 22:13:11 -0600

    cambios

--- src/service/utc.js
+++ src/service/utc.js
@@ -14,14 +14,14 @@ service.getUTCDiaActualFinal = function (format = false) {
   return moment().tz(config.timezone).endOf('day').utc().format(formato)
 }
 
-service.getUTCInicio = function (fecha,format = false) {
+service.getUTCInicio = function (fecha, format = false) {
   const formato = format || config.formatoUtc
-  return moment(fecha).tz(config.timezone).startOf('day').utc().format(formato)
+  return moment(fecha).startOf('day').tz(config.timezone).utc().format(formato)
 }
 
-service.getUTCFinal = function (fecha,format = false) {
+service.getUTCFinal = function (fecha, format = false) {
   const formato = format || config.formatoUtc
-  return moment(fecha).tz(config.timezone).endOf('day').utc().format(formato)
+  return moment(fecha).endOf('day').tz(config.timezone).utc().format(formato)
 }
 
 service.getUTCInicioMesActual = function (format = false) {
